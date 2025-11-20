f6e2d0b4e160faddc7f0aa538b3aa00883572048
dracotato <160530153+DracoTato@users.noreply.github.com>
2025-11-20 22:56:22 +0200

    refactor(styles): make the styles more readable & modifiable
    closes: #9

--- src/web/static/js/base.js
+++ src/web/static/js/base.js
@@ -1,14 +1,14 @@
 const sidebar = document.querySelector(".sidebar");
 const menuIcon = document.querySelector(".topbar .menu-icon");
-const closeIcon = document.querySelector(".sidebar .close-icon");
+const closeIcon = document.querySelector(".topbar .close-icon");
 
 menuIcon.addEventListener("click", openSidebar);
 closeIcon.addEventListener("click", closeSidebar);
 
-function closeSidebar() {
-  sidebar.style.left = "-100%";
+function openSidebar() {
+  sidebar.classList.add("open");
 }
 
-function openSidebar() {
-  sidebar.style.left = "0";
+function closeSidebar() {
+  sidebar.classList.remove("open");
 }
