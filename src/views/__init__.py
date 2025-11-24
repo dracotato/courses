524f23e81a207c27da2f660148ed608e6313f2f3
dracotato <160530153+DracoTato@users.noreply.github.com>
2025-11-25 01:56:42 +0200

    feat(frontend): add templates and css components for course entity operations

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, Flask, render_template
 
 from .auth import auth_bp
+from .course import course_bp
 from .lesson import lesson_bp
 
 root_bp: Blueprint = Blueprint("root", __name__)
-BLUEPRINTS = [root_bp, lesson_bp, auth_bp]
+BLUEPRINTS = [root_bp, lesson_bp, auth_bp, course_bp]
 
 
 def register_blueprints(app: Flask):
