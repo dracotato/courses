e5f742da1d147b779d624c5d1e05af07031969b5
dracotato <160530153+DracoTato@users.noreply.github.com>
2025-12-07 09:58:05 +0200

    feat(routes): show owned courses on the home page when logged-in
    closes: #11

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, g, render_template
+
+from src.db import db_execute
 
 from .auth import auth_bp
 from .course import course_bp
@@ -18,4 +20,13 @@ def register_blueprints(app):
 
 @root_bp.route("/")
 def index():
+    if g.get("user"):
+        courses = db_execute(
+            # select all courses that belong to current user and the number of lessons in each course
+            query="SELECT course.*, count(lesson.lessonid) AS lessoncount FROM course LEFT JOIN lesson ON course.courseid = lesson.course WHERE course.owner = ? GROUP BY course.courseid;",
+            params=(g.user["userid"],),
+            fetch_type=3,
+        )
+        return render_template("home.html", courses=courses)
+
     return render_template("home.html")
